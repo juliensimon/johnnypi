4dd3d83a845aabb15beea8294211d6f4173c19f7
Julien Simon <julien@julien.org>
2018-05-23 19:38:05 +0000

    Return language code only

--- part8/pi/ComprehendApi.py
+++ part8/pi/ComprehendApi.py
@@ -7,13 +7,11 @@ defaultRegion = 'eu-west-1'
 defaultUrl = 'https://comprehend.'+defaultRegion+'.amazonaws.com'
 defaultBucket = "jsimon-public"
 
-languages = {'en':'English', 'fr':'French', 'de':'German', 'es':'Spanish'}
-
 def connectToComprehend(regionName=defaultRegion, endpointUrl=defaultUrl):
     return boto3.client('comprehend', region_name=regionName, endpoint_url=endpointUrl)
 
 def detectLanguage(client,text):
         resp = client.detect_dominant_language(Text=text)
         language_code = resp['Languages'][0]['LanguageCode']
-	return language_code, languages[language_code]
+	return language_code
 
